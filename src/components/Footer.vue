8321426b0ff7f69564992966ca796af0cc9a445c
mariemsafwat20 <100482871+mariemsafwat20@users.noreply.github.com>
2022-03-02 21:22:09 +0200

    Added News Letter and News Letter localization

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,32 @@
 <script setup>
 import { useStore } from '../store'
 import { lists } from '../lang/navigation.json'
+import BaseInput from './Base/BaseInput.vue';
+import BaseTitle from './Base/BaseTitle.vue';
 
 const { $state: state } = $(useStore())
+
+const text = $ref({
+  title: {
+    ar: 'النشرة الإخبارية',
+    en: 'News Letter',
+  },
+  subTitle: {
+    ar: 'احصل علي المزادات الاسبوعيه',
+    en: 'Get Notified With Weekly New Bids',
+  },
+  emailplaceholder: {
+    ar: 'ادخل البريد الالكتروني',
+    en: 'Leave Your Email',
+  },
+})
 </script>
 
 <template>
   <footer
     class="mt-6 flex flex-col gap-5 border border-neutral-800 bg-bi-700 p-5 backdrop-blur-sm"
   >
-    <div class="flex items-start gap-10">
+    <div class="flex items-start gap-16 ">
       <ul v-for="(list, index) in lists" :key="index">
         <li class="mb-2 text-lg font-semibold text-gray-400">
           {{ $t(list.name) }}
@@ -20,6 +37,11 @@ const { $state: state } = $(useStore())
           </RouterLink>
         </li>
       </ul>
+      <div class="!w-full">
+        <BaseTitle class="!text-2xl mb-3">{{ $t(text.title) }}</BaseTitle>
+        <p class="mb-3 text-gray-300">{{ $t(text.subTitle) }}</p>
+        <BaseInput type="email" :placeholder="$t(text.emailplaceholder)" class="!w-full"/>
+      </div>
     </div>
     <div class="relative">
       <div class="mx-auto block w-fit bg-bi-700 px-3">
