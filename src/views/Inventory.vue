691e0d743ebcfb565d0ed6a684453b9b61218459
mariemsafwat20 <100482871+mariemsafwat20@users.noreply.github.com>
2022-03-11 19:05:26 +0200

    Change places editItem and newItem

--- src/views/Inventory.vue
+++ src/views/Inventory.vue
@@ -25,6 +25,14 @@ const text = $ref({
     ar: 'أضافه عنصر',
     en: 'Add item',
   },
+  editItem: {
+    ar: 'تعديل عنصر',
+    en: 'Edit item',
+  },
+  newItem: {
+    ar: 'عنصر جديد',
+    en: 'New item',
+  },
   namePlaceholder: {
     ar: 'الاسم',
     en: 'Name',
@@ -60,15 +68,7 @@ const text = $ref({
   no: {
     ar: 'لا',
     en: 'No',
-  },
-  editItem: {
-    ar: 'تعديل عنصر',
-    en: 'Edit item',
-  },
-  newItem: {
-    ar: 'عنصر جديد',
-    en: 'New item',
-  },
+  }
 })
 
 let items = $ref([
