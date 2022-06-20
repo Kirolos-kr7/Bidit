f1838f0faca84537abec7e3203d258f0166781a9
mariemsafwat20 <100482871+mariemsafwat20@users.noreply.github.com>
2022-06-20 17:37:22 +0200

    Edit Links For Facebook And Gmail

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -22,14 +22,14 @@ const text = $ref({
   <div class="my-5">
     <h1 class="font-serif text-lg">{{ $t(text.contact) }}</h1>
     
-    <a href="https://www.facebook.com"  class="p-1 flex items-center gap-3">
+    <a href="https://www.facebook.com/ElshoroukAcd/"  class="p-1 flex items-center gap-3">
       <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"><title>Facebook</title><path fill="#1877F2" d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
-      <span class="font-semibold text-indigo-800 transition-colors hover:text-indigo-500">Facebook</span>
+      <span class="font-semibold text-indigo-800 transition-colors hover:text-indigo-500">Shorouk Academy</span>
     </a>
     
-    <a href="https://www.gmail.com"  class="p-1 flex items-center gap-3">
+    <a href="bidit.platform@gmail.com"  class="p-1 flex items-center gap-3">
       <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6"><title>Gmail</title><path fill="#EA4335" d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"/></svg>
-      <span class="font-semibold text-rose-800 transition-colors hover:text-rose-500">Gmail</span>
+      <span class="font-semibold text-rose-800 transition-colors hover:text-rose-500">Bid!T</span>
     </a>
 
     <a href="https://www.linkedin.com/feed/"  class="p-1 flex items-center gap-3">
